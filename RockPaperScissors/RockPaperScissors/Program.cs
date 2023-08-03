3a7547a9cfdc31994b4aba3bc57e8862676d9e36
rebeccaP30 <79094275+rebeccaP30@users.noreply.github.com>
2023-08-03 15:23:08 +0200

    Add files via upload

---
+++ RockPaperScissors/RockPaperScissors/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace RockPaperScissors
+{
+    class Program
+    {
+        private const int Rock = 0;
+        private const int Paper = 1;
+        private const int Scissors = 2;
+
+        static void Main(string[] args)
+        {
+            Random randomNumbers = new Random();
+
+            string playerChoice;
+            int playerValue = -1;
+
+            do
+            {
+
+
+                int computerValue = randomNumbers.Next(3);
+                string computerChoice;
+
+                //The code for your solution goes below this comment
+                if (computerValue == Rock)
+                {
+                    computerChoice = "rock";
+                }
+                else if (computerValue == Paper)
+                {
+                    computerChoice = "paper";
+                }
+                else
+                {
+                    computerChoice = "sciccors";
+                }
+
+                Console.Clear();
+                Console.Write("Please enter rock, paper or scissors ");
+                playerChoice = Console.ReadLine().ToLower();
+
+                if (playerChoice.Equals("rock"))
+                {
+                    playerValue = Rock;
+                }
+                else if (playerChoice.Equals("paper"))
+                {
+                    playerValue = Paper;
+                }
+                else if (playerChoice.Equals("scissors"))
+                {
+                    playerValue = Scissors;
+                }
+                else
+                {
+                    Console.WriteLine($"{playerChoice} is not a valid choice");
+                }
+
+                Console.WriteLine($"Computer chose {computerChoice}, player chose {playerChoice}");
+                if (playerValue == computerValue)
+                {
+                    Console.WriteLine("It's a draw");
+                }
+                else if ((playerValue - 1 == computerValue) || (playerValue == Rock && computerValue == Scissors))
+                {
+                    Console.WriteLine("Player wins");
+                }
+                else
+                {
+                    Console.WriteLine("The computer wins!");
+                }
+            }   while (GetYesOrNo ("Would you like to play again"));
+
+        }
+        /**
+        * Returns a boolean response to a yes/no question.
+        *
+        * @param string
+        *            The question to be asked.
+        * @return True if the answer was yes, False if no.
+        */
+        public static bool GetYesOrNo(string question)
+        {
+            char answer;
+            while (true)  // infinite loop.  return will exit the method, thus terminating the loop
+            {
+                Console.Write($"{question} ");
+                answer = Console.ReadKey(true).KeyChar;
+                answer = char.ToLower(answer);
+                if (answer.Equals('y'))
+                    return true;
+                if (answer.Equals('n'))
+                    return false;
+            }
+        }
+
+    }
+}
